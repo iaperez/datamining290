8a748512bb5bac2659474e3b82f01068593977b7
Ignacio Perez <iaperez@berkeley.edu>
2013-02-28 22:34:50 -0800

    gini assignment

--- code/gini.py
+++ code/gini.py
@@ -2,6 +2,8 @@
 """Script can be used to calculate the Gini Index of a column in a CSV file.
 
 Classes are strings."""
+from collections import defaultdict
+from collections import Counter
 
 import fileinput
 import csv
@@ -12,22 +14,39 @@ receipt_desc, memo_cd, memo_text, form_tp, file_num, tran_id, election_tp) = ran
 
 
 ############### Set up variables
-# TODO: declare datastructures
+candidates =  defaultdict(Counter)
+zipcodes =  defaultdict(Counter)
 
 ############### Read through files
+############### I decided to cleanup the zipcodes, taking only the first 5 digits
 for row in csv.reader(fileinput.input()):
     if not fileinput.isfirstline():
-        ###
-        # TODO: replace line below with steps to save information to calculate
-        # Gini Index
-        row[cand_nm], row[contbr_zip]
-        ##/
+        candidates[row[cand_nm]][row[contbr_zip][:5]]+=1
+        zipcodes[row[contbr_zip][:5]][row[cand_nm]]+=1
+
+# for gini calculations, the total of contrbutors in the dataset
+totalcontributors = sum([sum(x.values()) for x in candidates.itervalues()]);
+
+
+probs_per_candidate =  defaultdict(float)
+for candidate in candidates.iterkeys():
+    probs_per_candidate[candidate]=float(sum(candidates[candidate].values()))/float(totalcontributors)
+
+def gini_per_zipcode(dict_of_contributors):
+    total_contributors_per_zip=sum(dict_of_contributors.values())
+    probs_per_candidate_in_zipcode = defaultdict(float)
+    for candidate in dict_of_contributors.keys():
+        probs_per_candidate_in_zipcode[candidate]=float(dict_of_contributors[candidate])/float(total_contributors_per_zip) 
+    return 1-sum(probs**2 for probs in probs_per_candidate_in_zipcode.values())
 
 ###
 # TODO: calculate the values below:
 gini = 0  # current Gini Index using candidate name as the class
 split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
 ##/
+
+gini = 1 - sum(probs**2 for probs in probs_per_candidate.values())
+split_gini = sum(sum(x.values())*gini_per_zipcode(x)/totalcontributors for x in zipcodes.values())
 
 print "Gini Index: %s" % gini
 print "Gini Index after split: %s" % split_gini
